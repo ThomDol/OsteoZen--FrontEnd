ae22347823855147a3e39abe2cb796cac75b01b2
ThomDol <thomas.dolige@gmail.com>
2024-07-05 09:13:06 +0200

    tom

--- src/components/Accueil/Accueil.jsx
+++ src/components/Accueil/Accueil.jsx
@@ -3,7 +3,7 @@ import NavBar from '../header/NavBar';
 import axios from'axios';
 import { useNavigate } from 'react-router-dom';
 import logoMassage from '../../assets/logoMassage.png';
-import { Link } from 'react-router-dom';
+
 
 
 const Accueil = () => {
