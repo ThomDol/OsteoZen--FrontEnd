92d9ebdf207e0de3142d997d0c107d9d52606566
ThomDol <thomas.dolige@gmail.com>
2024-07-08 08:41:29 +0200

    tom

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ import UpdatePraticien from "./components/module Admin/UpdatePraticien";
 function App() {
   return (
     <StorageProvider>
-      <div className="container-fluid" style={{marginLeft:"10%"}}>
+      <div className="container p-2" style={{marginLeft:"10%"}}>
         <Router>
           <Routes>
             <Route path="/login" element={<Login />} />
